c9b420990031f12455c46efc3d6a8cd22dd19aa9
Seikun Kambashi <seikun_lee@hotmail.com>
2016-12-04 23:31:02 +0900

    adding polls and cleaning up app.js

---
+++ models/poll.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose'),
+    ObjectID = require('mongodb').ObjectID;
+
+var voteSchema = mongoose.Schema({
+  user_id: String,
+  choice_id: String,
+  poll_id: String
+});
+
+var choiceSchema = mongoose.Schema({
+  text: String,
+  votes: [voteSchema]
+});
+
+var pollSchema = mongoose.Schema({
+  owner: String,
+  text: String,
+  choices: [choiceSchema]
+});
+
+var Poll = mongoose.model('Poll', pollSchema);
+
+exports.add_poll = function(pollOwner, pollName, callback) {
+  // callback signature for add_poll : function(err, newPoll)
+  var newPoll = new Poll({
+    owner: pollOwner,
+    text: pollName
+  });
+  newPoll.save(callback);
+};
+
+exports.remove_poll = function(p_id, callback) {
+  // callback signature for remove_poll: function(err)
+  Poll.remove({ _id: new ObjectID(p_id) }, callback);
+};
+
+exports.get_all_polls = function(callback) {
+  // callback signature for get_all_polls: function (err, polls)
+  Poll.find(callback);
+};
